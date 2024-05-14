a25b3396ee4c34551796a5dd6b2463a928d3da77
EgorMLL <151934967+EgorMLL@users.noreply.github.com>
2024-05-14 19:50:06 +0300

    Update validators.py

--- validators.py
+++ validators.py
@@ -26,36 +26,4 @@ def is_gpt_token_limit(messages, total_spent_tokens):
     return all_tokens, ""
 
 # проверяем, не превысил ли пользователь лимиты на преобразование аудио в текст
-def is_stt_block_limit(user_id, duration):
 
-    audio_blocks = math.ceil(duration / 15)
-    all_blocks = count_all_blocks(user_id)[0] + audio_blocks
-
-    if duration >= 30:
-        msg = "SpeechKit STT работает с голосовыми сообщениями меньше 30 секунд"
-        return None, msg
-
-    if all_blocks >= MAX_USER_STT_BLOCKS:
-        msg = f"Превышен общий лимит SpeechKit STT {MAX_USER_STT_BLOCKS}. Использовано {all_blocks} блоков. Доступно: {MAX_USER_STT_BLOCKS - all_blocks}"
-        return None, msg
-
-    return audio_blocks
-
-
-# проверяем, не превысил ли пользователь лимиты на преобразование текста в аудио
-def is_tts_symbol_limit(user_id, text):
-    text_symbols = len(text)
-
-    # Функция из БД для подсчёта всех потраченных пользователем символов
-    all_symbols = count_all_symbol(user_id)[0] + text_symbols
-
-    # Сравниваем all_symbols с количеством доступных пользователю символов
-    if all_symbols >= MAX_USER_TTS_SYMBOLS:
-        msg = f"Превышен общий лимит SpeechKit TTS {MAX_USER_TTS_SYMBOLS}. Использовано: {all_symbols} символов. Доступно: {MAX_USER_TTS_SYMBOLS - all_symbols}"
-        return None, msg
-
-    # Сравниваем количество символов в тексте с максимальным количеством символов в тексте
-    if text_symbols >= MAX_TTS_SYMBOLS:
-        msg = f"Превышен лимит SpeechKit TTS на запрос {MAX_TTS_SYMBOLS}, в сообщении {text_symbols} символов"
-        return None, msg
-    return len(text)
